cb8ce966e16d3f50d5a30e88afc5dfbde125f4a7
Clarisse A Campos <tetearruda17@gmail.com>
2026-06-22 10:58:56 -0300

    Fazendo o exercicio 1, ja consegui pegar os nomes

---
+++ prog-2/6_textFiles/1/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#define N 3
+typedef struct aluno{
+    char nome[60];
+    int n1,n2,n3;
+    /* data */
+}ALUNO;
+
+void getNames(ALUNO *alunos, FILE * f){
+    int c,h,i;
+    char buffer[70];
+    for(h = 0; h < N; h++){
+        i = 0;
+        while((c = getc(f)) != '\n'){
+            alunos[h].nome[i] = c;
+            i++;
+        }
+        alunos[h].nome[i++] = '\0';
+        fgets(buffer, sizeof(buffer), f);
+    }
+}
+
+int main(){
+    ALUNO alunos[3];
+
+    FILE *f = fopen("entrada.txt","r");
+    if(f == NULL){
+        printf("\nERRO!"); return 0;
+    }
+    getNames(alunos, f);
+
+    printf("\nNome: %s", alunos[0].nome);
+    printf("\nNome: %s", alunos[1].nome);
+    printf("\nNome: %s", alunos[2].nome);
+    /*---------------------------*/
+
+    fclose(f);
+    return 0;
+}
