d951236e5d4e5142e589725e20041ff40f76c02b
Clarisse A Campos <tetearruda17@gmail.com>
2026-06-24 14:06:39 -0300

    cansada

--- prog-2/7_binaryFiles/ex2/main.c
+++ prog-2/7_binaryFiles/ex2/main.c
@@ -1,5 +1,65 @@
 #include <stdio.h>
+
+int sizeArray(FILE *f){
+	int i, aux, qtd = 0;
+	while(fread(&aux,sizeof(int),1,f) == 1){
+		qtd++;
+	}
+	rewind(f);
+	return qtd;
+
+}
+
+int bubblesort(FILE *f, int * v, int qtd){
+	int i,j,aux;
+
+	for(i = 0;i < qtd;i++){
+		fread(&v[i],sizeof(int),1,f);
+	}
+	
+	for(i = 0;i < qtd; i++)
+	{
+		for(j = 0; j < qtd-1-i;j++)
+		{
+			if(v[j] > v[j+1]){
+				aux = v[j];
+				v[j] = v[j+1];
+				v[j+1] = aux; 
+			}
+		}
+	}
+	i = 0; aux = 0;
+	while(i < qtd){ 
+		if(v[i] != v[i+1]){
+			aux++;
+		}
+		i++;
+	}
+	return aux;
+}
+
 int main(){
-	printf("\nHello, World");
+	int originalQtd, qtdSemRepeticao;
+	FILE * f = fopen("ex2.dat","rb");
+	if(f == NULL){
+		printf("\nErro"); return 0;
+	}
+	originalQtd = sizeArray(f);
+
+	printf("\nTamanho original: %d\n", originalQtd);
+
+	int v1[originalQtd];
+
+	qtdSemRepeticao = bubblesort(f,v1, originalQtd);
+
+
+	//******************************************** */
+	printf("\nVetor ordenado: ");
+	for(int i = 0;i < originalQtd; i++){
+		printf("%d, ", v1[i]);
+	}
+	printf("\nQtd sem repeticao: %d\n",qtdSemRepeticao);
+
+	fclose(f);
 	return 0;
 }
