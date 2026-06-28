c07c34b8c18a2eb98b1da621fd3e6cb1888c417e
Clarisse A Campos <tetearruda17@gmail.com>
2026-06-28 15:00:31 -0300

    concluido, ex 9 e 10

--- prog-2/8_bitwise/009e10.c
+++ prog-2/8_bitwise/009e10.c
@@ -2,9 +2,19 @@
 int compactaHora(int hora,  int minuto, int segundo){
     return segundo | (minuto << 8) | (hora << 16);
 }
+void descompactaHora(int horaCampactada, int * hora, int * minuto, int *segundo){
+    *segundo = horaCampactada & 0xff;
+    *minuto = (horaCampactada >> 8) & 0xff;
+    *hora = (horaCampactada >> 16) & 0xff;
+}
 int main(){
     int h = 14,m = 32,s = 25;
+    int rar = compactaHora(h,m,s);
 
-    printf("\nCodec: %d\n", compactaHora(h,m,s));
+    printf("\nHora compactada: %b", rar);
+    printf("\n--------------\n");
+    h = 0;m = 0;s = 0;
+    descompactaHora(rar,&h,&m,&s);
+    printf("Hora descompactada: %d:%d:%d\n",h,m,s);
     return 0;
 }
