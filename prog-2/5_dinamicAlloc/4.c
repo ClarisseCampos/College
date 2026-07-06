3ba0455f5e5659bfed48c8381c49f607a6b3f99f
Clarisse A Campos <tetearruda17@gmail.com>
2026-07-06 06:59:06 -0300

    fixing fibonnacci series (ex 4)

--- prog-2/5_dinamicAlloc/4.c
+++ prog-2/5_dinamicAlloc/4.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int * fibonacci(int n){
 
-    int * fb = (int *) malloc(sizeof(int)*n);
+    int i, * fb = (int *) malloc(sizeof(int)*n);
     if(fb == NULL){
         printf("\nErro na alocacao"); return NULL;
     }
-    fb[0] = 0; fb[1] = 1;
-    printf("\n%d, %d", fb[0], fb[1]);
 
-    for(int i = 2;i < n; i++){
+    for(i = 0;i < n; i++){
+        fb[i] = 0;
+    }
+    fb[1] = 1;
+
+    for(i = 2;i < n; i++){
         fb[i] = fb[i-1] + fb[i-2];
     }
 
@@ -20,9 +24,9 @@ int main(){
     int i, n = 10;
     int * v = fibonacci(n);
 
-    printf("\nSerie de fibonacci:\n");
+    printf("\nSerie de fibonacci:");
     for(i = 0;i < n;i++){
-        printf("%d", v[i]);
+        printf("%d ", v[i]);
     }
 
     free(v);
